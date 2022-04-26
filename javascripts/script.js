912dd059bc4ebc0fbe902b32c6d32cd8967cb913
willyozq96 <54833317+willyozq96@users.noreply.github.com>
2022-04-26 14:13:17 +0800

    add files

---
+++ javascripts/script.js
@@ -0,0 +1,17 @@
+﻿src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+
+const uploadInput = document.querySelector('#upload-input');
+const previewImg = document.querySelector('.upload-image img');
+
+uploadInput.addEventListener('change', e => {
+    if (e.target.files.length > 0) {
+        const url = URL.createObjectURL(e.target.files[0]);
+        previewImg.src = url;
+    }
+})
+
+$('#topheader .navbar-nav a').on('click', function () {
+    $('#topheader .navbar-nav').find('li.active').removeClass('active');
+    $(this).parent('li').addClass('active');
+})
